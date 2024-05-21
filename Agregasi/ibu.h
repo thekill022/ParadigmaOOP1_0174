a49b47d3f1a0ad36b6c108d1e2911d8fd7f355df
thekill022 <gibfathoni@gmail.com>
2024-05-21 16:39:55 +0700

    deklarasi member variabel pada class ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -3,7 +3,11 @@
 
 //membuat class ibu pada file header ibu.h
 class Ibu {
+public:
+	//deklarasi member variabel pada class ibu
+	string nama;
+	vector<anak*> daftar_anak;
 
-}
+};
 
 #endif // !IBU_H
