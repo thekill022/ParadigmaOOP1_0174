cbdb353194078391277822d1efaa002c4c6db436
thekill022 <gibfathoni@gmail.com>
2024-05-21 18:09:45 +0700

    deklarasi member variabel dari class pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -8,7 +8,12 @@ class dokter;
 //deklarasi dan pembuatan class pasien
 class pasien {
 
-}
+public:
+	//deklarasi member variabel dari class pasien
+	string nama;
+	vector<dokter*> daftar_dokter;
+
+};
 
 int main()
 {
