f8f1e7d6ae48ce627ab178188e0f319b72f97484
thekill022 <gibfathoni@gmail.com>
2024-05-21 18:12:30 +0700

    pendefinisian prosedur - prosedur dari class dokter dan class pasien diluar class

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -51,6 +51,34 @@ public:
 
 };
 
+//pendefinisian prosedur - prosedur dari class dokter dan class pasien diluar class
+void pasien::tambahDoker(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+};
+
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter)
+	{
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDoker(this);
+}
+
+void dokter::cetakPasien() {
+	cout << "Daftar Pasien dari Dokter \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien)
+	{
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
 int main()
 {
     
