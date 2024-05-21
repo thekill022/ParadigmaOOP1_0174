3cd0208daeb6faf3674360fd7efab54cba134255
thekill022 <gibfathoni@gmail.com>
2024-05-21 16:25:02 +0700

    membuat constructor dan destructor class manusia

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -8,6 +8,13 @@ public:
 	string name;
 	Jantung varJantung;
 	
+	//membuat constructor dan destructor class manusia
+	Manusia(string pName) : name(pName) {
+		cout << name << "Hidup\n";
+	}
+	~Manusia() {
+		cout << name << "Mati\n";
+	}
 };
 
 #endif // !MANUSIA_H
